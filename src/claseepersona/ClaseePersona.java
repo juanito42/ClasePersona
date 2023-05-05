97e9ed33d7c0c190ea907bc3d3f38c404e72a5b8
juanito42 <juan.carri.cano@gmail.com>
2023-05-05 14:38:14 +0200

    Inicio Proyecto

---
+++ src/claseepersona/ClaseePersona.java
@@ -0,0 +1,28 @@
+
+package claseepersona;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+
+public class ClaseePersona {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nombre;
+        String apellidos;
+        String fechaNacimiento;
+        
+    }
+    
+}
+
+class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+}
+    
+
